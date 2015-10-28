e6cb7e485aa0a7f831bd15ee11f6c6a7d77b7ca6
Mario Celi <mceli@stackbuilders.com>
2015-10-28 08:37:16 -0500

    Fix reservation controller test

--- tests/ReservationControllerTest.php
+++ tests/ReservationControllerTest.php
@@ -3,6 +3,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\VacationProperty;
+use App\Reservation;
 use App\User;
 
 class ReservationControllerTest extends TestCase
@@ -30,17 +31,44 @@ class ReservationControllerTest extends TestCase
         ];
         $newProperty = new VacationProperty($propertyData);
         $newUser->properties()->save($newProperty);
+        $this->assertCount(0, Reservation::all());
+
+        $mockTwilioService = Mockery::mock('Services_Twilio')
+                                ->makePartial();
+        $mockTwilioAccount = Mockery::mock();
+        $mockTwilioMessages = Mockery::mock();
+        $mockTwilioAccount->messages = $mockTwilioMessages;
+        $mockTwilioService->account = $mockTwilioAccount;
+
+        $twilioNumber = config('services.twilio')['number'];
+        $mockTwilioMessages
+            ->shouldReceive('sendMessage')
+            ->with($twilioNumber, $newUser->fullNumber(), 'Some reservation message')
+            ->once();
+
+        $this->app->instance(
+            'Services_Twilio',
+            $mockTwilioService
+        );
 
         // When
         $response = $this->call(
             'POST',
-            route('reservation-create'),
-            ['description' => $validDescription,
-             'image_url' => $validImageUrl,
+            route('reservation-create', ['id' => $newProperty->id]),
+            ['message' => 'Some reservation message',
              '_token' => csrf_token()]
         );
 
         // Then
-        
+        $this->assertCount(1, Reservation::all());
+        $reservation = Reservation::first();
+        $this->assertEquals($reservation->message, 'Some reservation message');
+        $this->assertRedirectedToRoute('property-show', ['id' => $newProperty->id]);
+        $this->assertSessionHas('status');
+        $flashreservation = $this->app['session']->get('status');
+        $this->assertEquals(
+            $flashreservation,
+            "Sending your reservation request now."
+        );
     }
 }
